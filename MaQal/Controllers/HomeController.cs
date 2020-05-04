f3853b3d12f3b97c82fa111320a85a11f9b1c1bb
omarabdelaziz9395@gmail.com <omarabdelaziz9395@gmail.com>
2020-05-04 08:34:10 +0200

    Add project files.

---
+++ MaQal/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using MaQal.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MaQal.Data;
+using MaQal.Data.Repository;
+
+namespace MaQal.Home
+{
+    public class HomeController : Controller
+    {
+        private IRepository _repos;
+
+        public HomeController(IRepository repos)
+        {
+            _repos = repos;
+        }
+
+
+        public IActionResult Index()
+        {
+            var posts = _repos.GetAllPosts();
+            return View(posts);
+        }
+        public IActionResult Post(int id)
+        {
+            var post = _repos.GetPost(id);
+            return View(post);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+                return View(new Post());
+            else
+            {
+                var post = _repos.GetPost((int) id);
+                return View(post);
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Post post)
+        {
+            if (post.Id > 0)
+            {
+                _repos.UpdatePost(post);
+            }
+            else
+            {
+                _repos.AddPost(post);
+            }
+
+            if (await _repos.SaveChangesAsync())
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(post);
+            }
+        }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            _repos.RemovePost(id);
+            await _repos.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MaQal/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MaQal.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MaQal.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            :base(options)
+        {
+            
+        }
+        public DbSet<Post> Posts { get; set; }
+        
+    }
+}
